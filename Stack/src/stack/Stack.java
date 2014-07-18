65e2d56f18da34940eae75c5db8207308296cff0
diaz1202 <alejodiaz.1202@gmail.com>
2014-07-18 07:30:55 -0600

    Interfaz

--- Stack/src/stack/Stack.java
+++ Stack/src/stack/Stack.java
@@ -10,13 +10,6 @@ package stack;
  *
  * @author Alejo
  */
-public class Stack {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
+public interface Stack {
     
 }
